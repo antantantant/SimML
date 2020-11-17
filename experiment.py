8debfc9067efb9914b4ac7ac1cf19235fd26dd49
Harsha Lakamsani <harsha.lakamsani@gmail.com>
2020-11-17 00:12:55 -0700

    Updated Neural ODE FW to 11/2020 version

--- experiment.py
+++ experiment.py
@@ -17,9 +17,11 @@ from utils import RunningAverageMeter, accuracy
 from simulations import SwitchDataset
 # matplotlib for tkagg (tkinter) fw
 import matplotlib
+
 matplotlib.use("TkAgg")
 # NODE imports
 from models import NODEfunc, ODEBlock
+
 # force FloatTensor
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
@@ -36,70 +38,78 @@ parser.add_argument('--lr', type=float, default=0.1)  # learning rate
 parser.add_argument('--batch_size', type=int, default=40)  # batch size for training; default = 20
 parser.add_argument('--test_batch_size', type=int, default=40)  # batch size for validation and test; default = 20
 parser.add_argument('--save', type=str, default='./experiment1')  # save dir
-parser.add_argument('--debug', action='store_true') # save debug output
-parser.add_argument('--gpu', type=int, default=0) # activate gpu if available (not default)
-args = parser.parse_args() # add all args
+parser.add_argument('--debug', action='store_true')  # save debug output
+parser.add_argument('--gpu', type=int, default=0)  # activate gpu if available (not default)
+args = parser.parse_args()  # add all args
 
 # initializing experiment
 if __name__ == '__main__':
 
-    utils.makedirs(args.save) # create directory based on args
-    logger = utils.get_logger(logpath=os.path.join(args.save, 'logs'), filepath=os.path.abspath(__file__)) # initialize logger, save to file
-    logger.info(args) # specify information in logger
+    utils.makedirs(args.save)  # create directory based on args
+    logger = utils.get_logger(logpath=os.path.join(args.save, 'logs'),
+                              filepath=os.path.abspath(__file__))  # initialize logger, save to file
+    logger.info(args)  # specify information in logger
 
-    device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu') # specify device for model (CUDA gpu if available)
+    device = torch.device('cuda:' + str(
+        args.gpu) if torch.cuda.is_available() else 'cpu')  # specify device for model (CUDA gpu if available)
+
 
     # visualize the model
     def create_dataset(model_type):
         # create simulation dataset
         return {
             # add new models here
-            'switch': SwitchDataset(device, args.time_span, args.test_batch_size, args.dt, args.tol, args.tol) # initialze model dataset
+            'switch': SwitchDataset(device, args.time_span, args.test_batch_size, args.dt, args.tol, args.tol)
+            # initialze model dataset
         }[model_type]
 
-    physics_simulation = create_dataset(args.physics) # call prior fn to generate dataset per args
+
+    physics_simulation = create_dataset(args.physics)  # call prior fn to generate dataset per args
     # vis = utils.Visualization(dataset=physics_simulation)
     # vis.plot_data()
 
     # train a baseline Neural ODE model
     # NOTE: please distinguish between physical and surrogate (statistical) models
-    settings = {'odefunc': NODEfunc(2), # 2D
-                'device': device, # gpu/cpu if available
-                'rtol': args.tol, # tolerance
-                'atol': args.tol} # tolerance
-    surrogate_model = ODEBlock(settings).to(device) # used to approximate outcomes
+    settings = {'odefunc': NODEfunc(2),  # 2D
+                'device': device,  # gpu/cpu if available
+                'rtol': args.tol,  # tolerance
+                'atol': args.tol}  # tolerance
+    surrogate_model = ODEBlock(settings).to(device)  # used to approximate outcomes
 
     # save model info
-    logger.info(surrogate_model) # store surrogate info
-    logger.info('Number of parameters: {}'.format(utils.count_parameters(surrogate_model))) # store number of parameters from args
+    logger.info(surrogate_model)  # store surrogate info
+    logger.info('Number of parameters: {}'.format(
+        utils.count_parameters(surrogate_model)))  # store number of parameters from args
 
     # define loss
     # NOTE: using MSE loss
     criterion = nn.MSELoss().to(device)  # TODO: check loss definition
 
     # get data streamer
-    train_loader, test_loader, train_eval_loader = utils.get_data_loaders(physics_simulation, args.batch_size, args.test_batch_size) # data loaders
-    data_gen = utils.inf_generator(train_loader) # generate data based on train split
-    batches_per_epoch = int(args.test_batch_size / args.batch_size) # batches/epoch as ratio of test:total batch size
+    train_loader, test_loader, train_eval_loader = utils.get_data_loaders(physics_simulation, args.batch_size,
+                                                                          args.test_batch_size)  # data loaders
+    data_gen = utils.inf_generator(train_loader)  # generate data based on train split
+    batches_per_epoch = int(args.test_batch_size / args.batch_size)  # batches/epoch as ratio of test:total batch size
 
     # training process
-#####################################################################
+    #####################################################################
     # TODO: need to fine-tune the learning rate scheme?
     # learning function
     lr_fn = utils.learning_rate_with_decay(
-        args.lr, batches_per_epoch=batches_per_epoch, boundary_epochs=[60, 100, 140], # specify args, batches (by boundary values)
-        decay_rates=[1, 0.1, 0.01, 0.001] # specify decay rate
+        args.lr, batches_per_epoch=batches_per_epoch, boundary_epochs=[60, 100, 140],
+        # specify args, batches (by boundary values)
+        decay_rates=[1, 0.1, 0.01, 0.001]  # specify decay rate
     )
 
     # set up the optimizer
-    optimizer = torch.optim.SGD(surrogate_model.parameters(), lr=args.lr, momentum=0.9) # use SGD as optimization algo
+    optimizer = torch.optim.SGD(surrogate_model.parameters(), lr=args.lr, momentum=0.9)  # use SGD as optimization algo
 
     # params for accuracy
-    best_acc = 0 # best accuracy value
-    batch_time_meter = utils.RunningAverageMeter() # batch timing
-    f_nfe_meter = utils.RunningAverageMeter() # forward epoch accuracy
-    b_nfe_meter = utils.RunningAverageMeter() # batch epoch accuracy
-    end = time.time() # time duration of model training
+    best_acc = 0  # best accuracy value
+    batch_time_meter = utils.RunningAverageMeter()  # batch timing
+    f_nfe_meter = utils.RunningAverageMeter()  # forward epoch accuracy
+    b_nfe_meter = utils.RunningAverageMeter()  # batch epoch accuracy
+    end = time.time()  # time duration of model training
 
     # loop through num. of batches
     for itr in range(args.nepochs * batches_per_epoch):
@@ -108,39 +118,41 @@ if __name__ == '__main__':
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr_fn(itr)
 
-        optimizer.zero_grad() # zero-out gradients as initial spec
-        x = data_gen.__next__() # x = next iteration within data_gen
-        x0 = x[:,:,0]  # get the initial states
-        x0 = x0.to(device) # send initial states to gpu/cpu
+        optimizer.zero_grad()  # zero-out gradients as initial spec
+        x = data_gen.__next__()  # x = next iteration within data_gen
+        x0 = x[:, :, 0]  # get the initial states
+        x0 = x0.to(device)  # send initial states to gpu/cpu
         pred = surrogate_model(x0.float(), args.dt)  # output is batch_size * dim * time, excluding the initial state
         # compute MSE between physical and surrogate model
-        loss = criterion(pred, x[:,:,1:].float())  # exclude initial states since they are the same for pred and target
+        loss = criterion(pred,
+                         x[:, :, 1:].float())  # exclude initial states since they are the same for pred and target
 
         # monitor forward ODE steps
-        nfe_forward = surrogate_model.nfe # extract features
-        surrogate_model.nfe = 0 # initialize to 0
+        nfe_forward = surrogate_model.nfe  # extract features
+        surrogate_model.nfe = 0  # initialize to 0
 
         # compute gradient and do gradient descent
-        loss.backward() # accumulate gradient
-        optimizer.step() # update current gradient
+        loss.backward()  # accumulate gradient
+        optimizer.step()  # update current gradient
 
         # monitor adjoint steps
-        nfe_backward = surrogate_model.nfe # extract features
-        surrogate_model.nfe = 0 # initialize to 0
+        nfe_backward = surrogate_model.nfe  # extract features
+        surrogate_model.nfe = 0  # initialize to 0
 
-        batch_time_meter.update(time.time() - end) # add in elapsed time
+        batch_time_meter.update(time.time() - end)  # add in elapsed time
 
-        f_nfe_meter.update(nfe_forward) # forward epoch accuracy
-        b_nfe_meter.update(nfe_backward) # backward epoch accuracy
+        f_nfe_meter.update(nfe_forward)  # forward epoch accuracy
+        b_nfe_meter.update(nfe_backward)  # backward epoch accuracy
 
-        end = time.time() # restate end time
+        end = time.time()  # restate end time
 
         # conditional: proceed if iterations evenly divisible by batches/epoch
         if itr % batches_per_epoch == 0:
             # inference w/o gradient calc
             with torch.no_grad():
-                train_acc = accuracy(surrogate_model, args.dt, criterion, train_eval_loader, device) # training accuracy
-                val_acc = accuracy(surrogate_model, args.dt, criterion, test_loader, device) # validation accuracy
+                train_acc = accuracy(surrogate_model, args.dt, criterion, train_eval_loader,
+                                     device)  # training accuracy
+                val_acc = accuracy(surrogate_model, args.dt, criterion, test_loader, device)  # validation accuracy
                 # conditional: if validation beats train accuracy
                 if val_acc > best_acc:
                     # save the surrogate_model
@@ -156,7 +168,7 @@ if __name__ == '__main__':
                         b_nfe_meter.avg, train_acc, val_acc
                     )
                 )
-#####################################################################
+    #####################################################################
 
     # visualize surrogate model along with the physics simulation
 
@@ -165,16 +177,16 @@ if __name__ == '__main__':
     #                                 strict=False)
     # surrogate_model.eval()
 
-    dim, time_step = physics_simulation.dim() # specify the dimension, time step for currogate model
-    predict = np.empty((0, dim, time_step)) # prediction vector
+    dim, time_step = physics_simulation.dim()  # specify the dimension, time step for surrogate model
+    predict = np.empty((0, dim, time_step))  # prediction vector
     # per value in test data
     for x in test_loader:
-        target = x[:,:,1:].to(device).float() # target tensor
-        x0 = x[:,:,0].to(device) # x0 tensor
-        x = surrogate_model(x0.float(), args.dt).cpu().detach().numpy() # gather x tensor from surrogate model
-        x0 = np.expand_dims(x0.cpu().detach().numpy(), axis=2) # add dim as np vector
+        target = x[:, :, 1:].to(device).float()  # target tensor
+        x0 = x[:, :, 0].to(device)  # x0 tensor
+        x = surrogate_model(x0.float(), args.dt).cpu().detach().numpy()  # gather x tensor from surrogate model
+        x0 = np.expand_dims(x0.cpu().detach().numpy(), axis=2)  # add dim as np vector
         x = np.concatenate((x0, x), axis=2)  # add the initial state (x0) as dim of x
-        predict = np.concatenate((predict, x), axis=0) # prediction vector
+        predict = np.concatenate((predict, x), axis=0)  # prediction vector
 
-    vis = utils.Visualization(dataset=[physics_simulation, predict]) # visualize the prediction vector
-    vis.compare_data() # compare visualization data
+    vis = utils.Visualization(dataset=[physics_simulation, predict])  # visualize the prediction vector
+    vis.compare_data()  # compare visualization data
--- models.py
+++ models.py
@@ -17,55 +17,56 @@ class ODEBlock(nn.Module):
 
     # initialize settings
     def __init__(self, settings):
-        super(ODEBlock, self).__init__() # use inherited settings
+        super(ODEBlock, self).__init__()  # use inherited settings
         # various settings for odefunc params
         self.odefunc = settings['odefunc']
         self.device = settings['device']
         self.rtol = settings['rtol']
         self.atol = settings['atol']
-        self.integration_time = torch.tensor([0, 1]).float() # integration time tensor
+        self.integration_time = torch.tensor([0, 1]).float()  # integration time tensor
 
     # forward iteration specs
     def forward(self, x, dt):
         # need to treat all intermediate ode steps as part of the graph
-        batch_size, dim = x.shape # initialize batch, dim sizes
-        out_seq = torch.empty(batch_size, dim, 0) # output sequence defined as tensor
-        n_step = int(1. / dt) # current step value
-        integration_time = torch.tensor([0, dt]).float() # initialize integration_time
+        batch_size, dim = x.shape  # initialize batch, dim sizes
+        out_seq = torch.empty(batch_size, dim, 0)  # output sequence defined as tensor
+        n_step = int(1. / dt)  # current step value
+        integration_time = torch.tensor([0, dt]).float()  # initialize integration_time
         # loop to iterate through values in n_step (number of steps)
         for i in range(n_step):
-            x_new = odeint(self.odefunc, x, integration_time, rtol=self.rtol, atol=self.atol)[1] # x_new tensor
-            x = x_new # x tensor = prior x_new tensor
+            x_new = odeint(self.odefunc, x, integration_time, rtol=self.rtol, atol=self.atol)[1]  # x_new tensor
+            x = x_new  # x tensor = prior x_new tensor
             out_seq = torch.cat((out_seq, x_new.unsqueeze(-1)), 2)  # dim 1: batch, dim 2: state dim, dim 3: time
-        return out_seq # output is out_seq
+        return out_seq  # output is out_seq
 
     # step fn specs
     def step(self, x, dt):
-        integration_time = torch.tensor([0, dt]).float() # initialze integration_time
-        x_new = odeint(self.odefunc, x, integration_time, rtol=self.rtol, atol=self.atol)[1].cpu().detach().numpy() x_new tensor
-        return x_new # output is x_new
+        integration_time = torch.tensor([0, dt]).float()  # initialze integration_time
+        x_new = odeint(self.odefunc, x, integration_time, rtol=self.rtol, atol=self.atol)[
+            1].cpu().detach().numpy()  # x_new tensor
+        return x_new  # output is x_new
 
     # simulate fn specs
     def simulate(self, x, dt):
-        out_seq = np.expand_dims(x.cpu().detach().numpy(), axis=2) # add dimension
-        n_step = int(1. / dt) # initialze n_step
+        out_seq = np.expand_dims(x.cpu().detach().numpy(), axis=2)  # add dimension
+        n_step = int(1. / dt)  # initialze n_step
         # loop to iterate through values in n_step
         for i in range(n_step):
-            x_new = self.step(x, dt) # initialize x_new
-            x = torch.tensor(x_new).to(self.device).float() # x tensor
+            x_new = self.step(x, dt)  # initialize x_new
+            x = torch.tensor(x_new).to(self.device).float()  # x tensor
             x_new = np.expand_dims(x_new, axis=2)  # add time as the 3rd dimension
             out_seq = np.concatenate((out_seq, x_new), axis=2)  # dim 1: batch, dim 2: state dim, dim 3: time
-        return out_seq # output is out_seq
+        return out_seq  # output is out_seq
 
-    @property # nfe decorator
+    @property  # nfe decorator
     # nfe fn
     def nfe(self):
-        return self.odefunc.nfe # output odefn, nfe
+        return self.odefunc.nfe  # output odefn, nfe
 
-    @nfe.setter # nfe decorator
+    @nfe.setter  # nfe decorator
     # nfe fn overriding
     def nfe(self, value):
-        self.odefunc.nfe = value # overwrite odefn, nfe as value param
+        self.odefunc.nfe = value  # overwrite odefn, nfe as value param
 
 
 # switch model: single particle, deterministic, 2D, switch between two ODE models
@@ -73,7 +74,7 @@ class SwitchBlock(nn.Module):
 
     # initialize with params
     def __init__(self, settings):
-        super(SwitchBlock, self).__init__() # call inherited settings
+        super(SwitchBlock, self).__init__()  # call inherited settings
         # various ode specss
         self.odefunc1 = settings['odefunc1']
         self.odefunc2 = settings['odefunc2']
@@ -91,10 +92,11 @@ class SwitchBlock(nn.Module):
 
     # step fn
     def step(self, x, dt):
-        integration_time = torch.tensor([0, dt]).float() # initialize integration_time
-        ind = self.region(x) # index
-        if x[ind].shape[0] > 0:   # if not empty
-            x[ind] = odeint(self.odefunc1, x[ind], integration_time, rtol=self.rtol, atol=self.atol)[1] # load specific odeint
+        integration_time = torch.tensor([0, dt]).float()  # initialize integration_time
+        ind = self.region(x)  # index
+        if x[ind].shape[0] > 0:  # if not empty
+            x[ind] = odeint(self.odefunc1, x[ind], integration_time, rtol=self.rtol, atol=self.atol)[
+                1]  # load specific odeint
         ind = torch.logical_not(ind) + 1  # flip indices
         if x[ind].shape[0] > 0:  # if not empty
             x[ind] = odeint(self.odefunc2, x[ind], integration_time, rtol=self.rtol, atol=self.atol)[1]
@@ -102,30 +104,30 @@ class SwitchBlock(nn.Module):
 
     # simulate fn
     def simulate(self, x, dt):
-        out_seq = np.expand_dims(x.cpu().detach().numpy(), axis=2) # add dimension to out_seq
-        out_ind_seq = self.region(x).cpu().detach().numpy() # define out_ind_seq as np vector
-        n_step = int(1. / dt) # initialize n_step
+        out_seq = np.expand_dims(x.cpu().detach().numpy(), axis=2)  # add dimension to out_seq
+        out_ind_seq = self.region(x).cpu().detach().numpy()  # define out_ind_seq as np vector
+        n_step = int(1. / dt)  # initialize n_step
         # loop through values in n_step
         for i in range(n_step):
-            x_new, ind = self.step(x, dt) # x_new and ind as output of step fn
-            x = torch.tensor(x_new).to(self.device) # x tensor
+            x_new, ind = self.step(x, dt)  # x_new and ind as output of step fn
+            x = torch.tensor(x_new).to(self.device)  # x tensor
             x_new = np.expand_dims(x_new, axis=2)  # add time as the 3rd dimension
             out_seq = np.concatenate((out_seq, x_new), axis=2)  # dim 1: batch, dim 2: state dim, dim 3: time
             out_ind_seq = np.vstack((out_ind_seq, ind))  # to be transposed to: dim 1: batch, dim 2: time
-        return out_seq, out_ind_seq.T # outputs are out_seq, transposed out_ind_seq
+        return out_seq, out_ind_seq.T  # outputs are out_seq, transposed out_ind_seq
 
-    @staticmethod # region decorator
+    @staticmethod  # region decorator
     # region fn
     def region(x):
         return x[:, 0] < 0.5  # TODO: formalize this
 
-    @property # nfe decorator
+    @property  # nfe decorator
     def nfe(self):
-        return self.odefunc.nfe # return odefunc's nfe
+        return self.odefunc.nfe  # return odefunc's nfe
 
-    @nfe.setter # nfe decorator
+    @nfe.setter  # nfe decorator
     def nfe(self, value):
-        self.odefunc.nfe = value # set odefunc's nfe as input value param
+        self.odefunc.nfe = value  # set odefunc's nfe as input value param
 
 
 # define physically explainable ODE module
@@ -134,15 +136,15 @@ class LinearODEfunc(nn.Module):
     # TODO: odeint with control inputs?
     # initializing odefunc
     def __init__(self, A):
-        super(LinearODEfunc, self).__init__() # call inherited settings
+        super(LinearODEfunc, self).__init__()  # call inherited settings
         self.A = torch.tensor(A).float()  # first dim is batch, need float for matrix mul
-        self.nfe = 0 # nfe initialized to 0
+        self.nfe = 0  # nfe initialized to 0
 
     # forward fn
     def forward(self, t, x):
-        self.nfe += 1 # increment nfe
-        out = torch.mm(self.A, torch.transpose(x.float(), 0, 1)) # matrix multiplication of A, transposed x tensor
-        return torch.transpose(out, 0, 1).double() # output is out tensor
+        self.nfe += 1  # increment nfe
+        out = torch.mm(self.A, torch.transpose(x.float(), 0, 1))  # matrix multiplication of A, transposed x tensor
+        return torch.transpose(out, 0, 1).double()  # output is out tensor
 
 
 # define NN model to mimic ODE
@@ -151,20 +153,20 @@ class NODEfunc(nn.Module):
     # this model can be customized
     # initializing odefunc
     def __init__(self, dim):
-        super(NODEfunc, self).__init__() # call inherited settings
-        self.linear1 = nn.Linear(dim, 16) # input layer (1)
-        self.linear2 = nn.Linear(16, 32) # intermediate layer (2)
-        self.linear3 = nn.Linear(32, dim) # output layer (3)
-        self.relu = nn.ReLU(inplace=True) # rectified linear unit
-        self.nfe = 0 # initialize nfe
+        super(NODEfunc, self).__init__()  # call inherited settings
+        self.linear1 = nn.Linear(dim, 16)  # input layer (1)
+        self.linear2 = nn.Linear(16, 32)  # intermediate layer (2)
+        self.linear3 = nn.Linear(32, dim)  # output layer (3)
+        self.relu = nn.ReLU(inplace=True)  # rectified linear unit
+        self.nfe = 0  # initialize nfe
 
     # forward fn
     def forward(self, t, x):
-        self.nfe += 1 # increment nfe
+        self.nfe += 1  # increment nfe
         # iterative outputs using NODE layers (3 sets)
         out = self.linear1(x)
         out = self.relu(out)
         out = self.linear2(out)
         out = self.relu(out)
         out = self.linear3(out)
-        return out # output NODE result
+        return out  # output NODE result
